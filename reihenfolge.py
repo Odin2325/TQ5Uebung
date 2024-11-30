8862f429a0f1f69996ef6e2b65e9e2eaa1532b7f
samo <155889897+samo-1@users.noreply.github.com>
2024-11-30 10:48:55 +0100

    Added reihenfolge.py to shuffle the list of students to a random order

---
+++ reihenfolge.py
@@ -0,0 +1,20 @@
+# pick students in a random order
+
+import random
+
+students = [
+                'Afsoon',
+                'Andreas',
+                'Dominic',
+                'Hugo',
+                'Nabila',
+                'Gökhan',
+                'Hüseyin',
+                'Ronny',
+                'Sonja',
+                'Stalone'
+            ]
+
+random.shuffle(students)
+
+print(students)
