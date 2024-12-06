95f09d2e3eca0be1903620a54b7e66aa6d026089
Odin2325 <nico.arevalo2325@gmail.com>
2024-12-06 12:00:08 +0100

    Dezember 6

--- ProgrammierenLernen/ProgrammierenLernen/Program.cs
+++ ProgrammierenLernen/ProgrammierenLernen/Program.cs
@@ -2,11 +2,30 @@
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
 
-
-            
         }
+
+        internal static void SummeZufallsZahlen()
+        {
+            Random zufallszahlGenerator = new Random();
+
+            Program.Summe(zufallszahlGenerator.Next(1, 301), zufallszahlGenerator.Next(1, 301), zufallszahlGenerator.Next(1, 301));
+        }
+
+        internal static void Summe(int a, int b, int c)
+        {
+            Console.WriteLine($"Die Summe von {a} + {b} + {c} ist: {a+b+c}");
+        }
+
+        internal static void Begruessung(string name)
+        {
+            Console.WriteLine($"Wilkommen im Kurs {name}!");
+            Program.Summe(10,25,20);
+        }
+
+
     }
 }
