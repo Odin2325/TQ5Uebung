6f2eeb55e71af65a3b860dab668093dd40f7b11d
Odin2325 <nico.arevalo2325@gmail.com>
2024-12-09 13:23:23 +0100

    MovedFile

--- ProgrammierenLernen/ProgrammierenLernen/obj/Debug/net8.0/ProgrammierenLernen.AssemblyInfo.cs
+++ ProgrammierenLernen/ProgrammierenLernen/obj/Debug/net8.0/ProgrammierenLernen.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProgrammierenLernen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95f09d2e3eca0be1903620a54b7e66aa6d026089")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f29d8433768d56ea5d28289f0d7277d6173f5461")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProgrammierenLernen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProgrammierenLernen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
