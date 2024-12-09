5a5ed7b1b6d82c2b1df46212e132da37f82dd2e3
samo-1 <155889897+samo-1@users.noreply.github.com>
2024-12-09 07:54:06 +0100

    Übungen Programmieren 2

--- Aufgaben/EingabenProgrammieren/Program.cs
+++ Aufgaben/EingabenProgrammieren/Program.cs
@@ -4,7 +4,9 @@ while (true)
     Console.WriteLine("Temperaturumrechnung: 1");
     Console.WriteLine("Zylinderberechnung:   2");
     Console.WriteLine("Zinseszins:           3");
+    Console.WriteLine("Großbuchstaben:       4");
     Console.Write("Wähle: ");
+
     string choice = Console.ReadLine();
 
     switch (choice)
@@ -17,6 +19,9 @@ while (true)
             break;
         case "3":
             Zinseszins();
+            break;
+        case "4":
+            Großbuchstaben();
             break;
         default:
             return;
@@ -31,9 +36,14 @@ void Temperaturumrechnung()
 
     if (!Eingabe(out celsius)) return;
 
-    decimal fahrenheit = celsius * 1.8m + 32;
+    decimal fahrenheit = CelsiusToFahrenheit(celsius);
 
     Console.WriteLine($"{celsius} °C sind {fahrenheit} °F");
+}
+
+decimal CelsiusToFahrenheit(decimal celsius)
+{
+    return celsius * 1.8m + 32;
 }
 
 void Zylinderberechnung()
@@ -47,31 +57,60 @@ void Zylinderberechnung()
     Console.Write("Radius des Zylinders: ");
     if (!Eingabe(out radius)) return;
 
-    decimal huelle = 2 * radius * hoehe * (decimal)Math.PI;
-    decimal oberflaeche = 2 * radius * (decimal)Math.PI * (radius + hoehe);
-    decimal volumen = 2 * radius * hoehe * (decimal)Math.PI;
-
+    decimal huelle = ZylinderHuelle(radius, hoehe);
+    decimal oberflaeche = ZylinderOberfläche(radius, hoehe);
+    decimal volumen = ZylinderVolumen(radius, hoehe);
 
     Console.WriteLine($"Die Hülle ist {huelle}, die Gesamtoberfläche ist {oberflaeche} und das Volumen ist {volumen}");
 }
+
+decimal ZylinderHuelle(decimal radius, decimal hoehe)
+{
+    return 2 * radius * hoehe * (decimal)Math.PI;
+}
+
+decimal ZylinderOberfläche(decimal radius, decimal hoehe)
+{
+    return 2 * radius * (decimal)Math.PI * (radius + hoehe);
+}
+
+decimal ZylinderVolumen(decimal radius, decimal hoehe)
+{
+    return 2 * radius * hoehe * (decimal)Math.PI;
+}
+
 void Zinseszins()
 {
-    return;
+    decimal n = 1m;
+    decimal r = 0.04m;
+    decimal P;
+    decimal t;
 
-    // N?
+    Console.Write("Anfangsinvestition: ");
+    if (!Eingabe(out P)) return;
 
-    //decimal P;
-    //decimal t;
+    Console.Write("Dauer: ");
+    if (!Eingabe(out t)) return;
 
-    //Console.Write("Anfangsinvestition: ");
-    //if (!Eingabe(out P)) return;
+    decimal A = ZinsesZinsBerechnen(P, r, t, n);
 
-    //Console.Write("Dauer: ");
-    //if (!Eingabe(out t)) return;
+    Console.WriteLine(
+            $"Nach Anlegen von {P} über {n} Jahre bei einem Zinssatz von " +
+            $"{r} p.a. sind auf dem Konto: {A}"
+        );
+}
 
-    //decimal A = P(1+)
+decimal ZinsesZinsBerechnen(decimal P, decimal r, decimal t, decimal n)
+{
+    return P * (decimal)Math.Pow((double)((1 + r / n)), (double)(n * t));
+}
 
-    //Console.WriteLine($"{}")
+void Großbuchstaben()
+{
+    Console.WriteLine("Was möchtest du konvertieren? ");
+    string eingabe = Console.ReadLine();
+
+    Console.WriteLine(eingabe.ToUpper());
 }
 
 bool Eingabe(out decimal eingabe)
