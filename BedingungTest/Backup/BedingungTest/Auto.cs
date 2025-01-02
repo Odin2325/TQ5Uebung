b85a6af3be5e1f25ebf6f11058136d2a80198156
afsoontq5test <afsoonrezaei@gmail.com>
2025-01-02 23:00:05 +0100

    Auto Class
    Commit class

---
+++ BedingungTest/Backup/BedingungTest/Auto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BedingungTest
+{
+    internal class Auto
+    {
+
+
+        internal string farbe;
+        internal string hersteller;
+        internal string modell;
+        internal int maxGeschwindigkeit;
+        internal bool hat4WD;
+        internal int baujahr;
+        internal string kraftstoffart;
+        internal int sitzanzahl;
+           
+        internal Auto(string farbe, string hersteller, string modell, int maxGeschwindigkeit, bool hat4WD, int baujahr, string kraftstoffart, int sitzanzahl)
+        {
+            this.farbe = farbe;
+            this.hersteller = hersteller;
+            this.modell = modell;
+            this.maxGeschwindigkeit = maxGeschwindigkeit;
+            this.hat4WD = hat4WD;
+            this.baujahr = baujahr;
+            this.kraftstoffart = kraftstoffart;
+            this.sitzanzahl = sitzanzahl;
+        }
+    }
+}
