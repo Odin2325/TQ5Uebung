6753d7668c91eb5bbdc1dc22a2b355b9236d497e
Odin2325 <nico.arevalo2325@gmail.com>
2025-01-13 18:11:25 +0100

    dbFiles

---
+++ TestingSQLite/TestingSQLite/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestingSQLite.Models;
+using TestingSQLite.Models;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Client> Clients { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Customize table and column mappings (optional)
+        modelBuilder.Entity<Client>().HasKey(c => c.ID); // Primary Key
+    }
+}
---
+++ TestingSQLite/TestingSQLite/Models/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingSQLite.Models
+{
+    public class Client
+    {
+        public int ID { get; set; } // Primary key
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
